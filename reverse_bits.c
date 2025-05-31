a1ac7b6745996e4e925411146d2aa51f3b1c732e
H N Keshava <155640771+dvsdd@users.noreply.github.com>
2025-05-31 06:27:41 +0530

    Update reverse_bits.c

--- reverse_bits.c
+++ reverse_bits.c
@@ -2,8 +2,8 @@
 uint32_t reverse(usint32_t n){
   uint32_t res = 0;
   for(int i=0;i<32;i++){
-    uint32_t bit = (n<<i)&1;
-    res = res|(bit <<(31-i);;
+    uint32_t bit = (n<<i)&1;   //extract bit
+    res = res|(bit <<(31-i);   //add it to the end of res
   }
   return res;
 }
